406d4ff6b2e6f6b88203ddf338a8594625b2c749
arturFormanchuk <4manchuk@gmail.com>
2020-03-21 18:47:28 +0200

    Updates

---
+++ precache-manifest.1b399e33230cb53f42adce97e5c5218b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55956c5d1208312cc47b16c0aac7afcf",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "02de5b15698f069d0b4d",
+    "url": "/my-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8125942f5854b2e33edc",
+    "url": "/my-app/static/css/main.aa88bc54.chunk.css"
+  },
+  {
+    "revision": "02de5b15698f069d0b4d",
+    "url": "/my-app/static/js/2.859b4bd5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-app/static/js/2.859b4bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8125942f5854b2e33edc",
+    "url": "/my-app/static/js/main.8ab4ac6f.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
